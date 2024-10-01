fd371ea11827f5ecbbe8d2a05b098367997b563c
Humberto Ribeiro <betofoxmal@gmail.com>
2024-09-30 21:40:20 -0300

    add providers diferente de http://localhost usar https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (config('app.env') !== 'http://localhost') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
